10b331fbd6ae3567ceb9e48a234d27ae51d3195e
Muhammad Mahad Khan <itzmahad01@gmail.com>
2026-06-05 12:10:48 +0500

    Update server.js

--- server.js
+++ server.js
@@ -21,10 +21,19 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
 
+// Root API
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "Backend is Live 🚀"
+  });
+});
+
+// Product Routes
 app.use("/", productRoute);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+
+
+export default app;
